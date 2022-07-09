5c5201f368260bc2804fdeecadc0bd751f8b50a5
jkim8 <juno0218@naver.com>
2022-07-09 19:41:03 -0400

    앱 꼈다켜도 로그인 유지하기

--- AppInner.tsx
+++ AppInner.tsx
@@ -8,9 +8,14 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {useSelector} from 'react-redux';
 import {RootState} from './src/store/reducer';
+import useSocket from './src/hooks/useSocket';
 import {useEffect} from 'react';
-import useSocket from './src/hooks/useSoket';
-
+import EncryptedStorage from 'react-native-encrypted-storage';
+import {useAppDispatch} from './src/store';
+import axios, {AxiosError} from 'axios';
+import Config from 'react-native-config';
+import userSlice from './src/slices/user';
+import {Alert} from 'react-native';
 export type LoggedInParamList = {
   Orders: undefined;
   Settings: undefined;
@@ -27,20 +32,24 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function AppInner() {
+  const dispatch = useAppDispatch();
+  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+  console.log('isLoggedIn', isLoggedIn);
+
   const [socket, disconnect] = useSocket();
 
   useEffect(() => {
-    const helloCallback = (data: any) => {
+    const callback = (data: any) => {
       console.log(data);
+      dispatch(orderSlice.actions.addOrder(data));
     };
     if (socket && isLoggedIn) {
-      console.log(socket);
-      socket.emit('login', 'hello');
-      socket.on('hello', helloCallback);
+      socket.emit('acceptOrder', 'hello');
+      socket.on('order', callback);
     }
     return () => {
       if (socket) {
-        socket.off('hello', helloCallback);
+        socket.off('order', callback);
       }
     };
   }, [isLoggedIn, socket]);
@@ -52,15 +61,41 @@ function AppInner() {
     }
   }, [isLoggedIn, disconnect]);
 
-  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
-  console.log('isLoggedIn', isLoggedIn);
-
+  // 앱 실행 시 토큰 있으면 로그인하는 코드
   useEffect(() => {
-    if (!isLoggedIn) {
-      console.log('!isLoggedIn', !isLoggedIn);
-    }
-  }, [isLoggedIn]);
-
+    const getTokenAndRefresh = async () => {
+      try {
+        const token = await EncryptedStorage.getItem('refreshToken');
+        if (!token) {
+          return;
+        }
+        const response = await axios.post(
+          `${Config.API_URL}/refreshToken`,
+          {},
+          {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        dispatch(
+          userSlice.actions.setUser({
+            name: response.data.data.name,
+            email: response.data.data.email,
+            accessToken: response.data.data.accessToken,
+          }),
+        );
+      } catch (error) {
+        console.error(error);
+        if ((error as AxiosError).response?.data.code === 'expired') {
+          Alert.alert('알림', '다시 로그인 해주세요.');
+        }
+      } finally {
+        //TODO: 스플래시 스크린 없애기
+      }
+    };
+    getTokenAndRefresh();
+  }, [dispatch]);
   return isLoggedIn ? (
     <Tab.Navigator>
       <Tab.Screen
