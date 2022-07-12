92a576d747508257e11b3a692cf850d2fc3542ef
jkim8 <juno0218@naver.com>
2022-07-11 20:41:32 -0400

    TextInput 옵션 추가

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import {
   Alert,
   Pressable,
@@ -7,50 +7,89 @@ import {
   TextInput,
   View,
 } from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../App';
 
-function SignIn() {
+type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
+
+function SignIn({navigation}: SignInScreenProps) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const emailRef = useRef<TextInput | null>(null);
+  const passwordRef = useRef<TextInput | null>(null);
+
   const onChangeEmail = useCallback(text => {
-    setEmail(text);
+    setEmail(text.trim());
   }, []);
   const onChangePassword = useCallback(text => {
-    setPassword(text);
+    setPassword(text.trim());
   }, []);
   const onSubmit = useCallback(() => {
-    Alert.alert('알림', '안녕');
-  }, []);
+    if (!email || !email.trim()) {
+      return Alert.alert('알림', '이메일을 입력해주세요.');
+    }
+    if (!password || !password.trim()) {
+      return Alert.alert('알림', '비밀번호를 입력해주세요.');
+    }
+    Alert.alert('알림', '로그인 되었습니다.');
+  }, [email, password]);
+
+  const toSignUp = useCallback(() => {
+    navigation.navigate('SignUp');
+  }, [navigation]);
 
   const canGoNext = email && password;
-
   return (
     <View>
-      <View>
-        <Text>이메일</Text>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이메일</Text>
         <TextInput
-          placeholder="이메일을 입력해 주세요."
-          onChange={onChangeEmail}
+          style={styles.textInput}
+          onChangeText={onChangeEmail}
+          placeholder="이메일을 입력해주세요"
+          placeholderTextColor="#666"
+          importantForAutofill="yes"
+          autoComplete="email"
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          value={email}
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          ref={emailRef}
+          onSubmitEditing={() => passwordRef.current?.focus()}
+          blurOnSubmit={false}
         />
       </View>
-      <View>
-        <Text>비밀번호</Text>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>비밀번호</Text>
         <TextInput
-          placeholder="비밀번호를 입력해 주세요."
-          onChange={onChangePassword}
+          style={styles.textInput}
+          placeholder="비밀번호를 입력해주세요(영문,숫자,특수문자)"
+          placeholderTextColor="#666"
+          importantForAutofill="yes"
+          onChangeText={onChangePassword}
+          value={password}
+          autoComplete="password"
+          textContentType="password"
+          secureTextEntry
+          returnKeyType="send"
+          clearButtonMode="while-editing"
+          ref={passwordRef}
+          onSubmitEditing={onSubmit}
         />
       </View>
       <View style={styles.buttonZone}>
         <Pressable
           style={
-            !canGoNext
-              ? styles.loginButton
-              : StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
+            canGoNext
+              ? StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
+              : styles.loginButton
           }
-          onPress={onSubmit}
-          disabled={!canGoNext}>
+          disabled={!canGoNext}
+          onPress={onSubmit}>
           <Text style={styles.loginButtonText}>로그인</Text>
         </Pressable>
-        <Pressable>
+        <Pressable onPress={toSignUp}>
           <Text>회원가입하기</Text>
         </Pressable>
       </View>
@@ -59,6 +98,21 @@ function SignIn() {
 }
 
 const styles = StyleSheet.create({
+  textInput: {
+    padding: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  inputWrapper: {
+    padding: 20,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  buttonZone: {
+    alignItems: 'center',
+  },
   loginButton: {
     backgroundColor: 'gray',
     paddingHorizontal: 20,
@@ -69,8 +123,10 @@ const styles = StyleSheet.create({
   loginButtonActive: {
     backgroundColor: 'blue',
   },
-  loginButtonText: {color: 'white'},
-  buttonZone: {alignItems: 'center'},
+  loginButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
 
 export default SignIn;
